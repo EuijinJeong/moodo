f0389f38ff93216cf4a8bb75caa7202a701b9081
EuijinJeong <jej001228@gmail.com>
2024-06-24 12:01:22 +0900

    제품 추천기능 구현 완료

--- frontend/src/components/ProductRecommendation.jsx
+++ frontend/src/components/ProductRecommendation.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ProductItem from './ProductItem';
-import '../css/product_recommendation.css'; // Include CSS for styling
+import '../css/product_recommendation.css';
+import axios from "axios"; // Include CSS for styling
 
-const products = [
-    { id: 1, name: '치이카와 인형 25cm', price: '9,000원', time: '21시간 전', image: 'path/to/image1.jpg' },
-    { id: 2, name: '스웨이드 미들부츠', price: '20,000원', time: '2일 전', image: 'path/to/image2.jpg' },
-    // 데이터베이스에서 저장된 아이템 불러와서 여기 변수에다가 저장해서 화면에 띄워야 함.
-];
 
 const ProductRecommendation = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchRandomProducts = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await axios.get('/api/products/random', {
+
+                    headers : {
+                        Authorization: `Bearer ${token}`
+                    },
+                    params: {count: 5}
+                });
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Failed to fetch random products', error);
+            }
+        }
+        fetchRandomProducts();
+    }, []);
+
     return (
         <section className="product-recommendation">
             <h2>오늘의 상품 추천</h2>
             <div className="product-list">
-                {products.map(product => (
-                    <ProductItem key={product.id} product={product} />
-                ))}
+                {products.length > 0 ? (
+                    products.map(product => (
+                        <ProductItem key={product.id} product={product} />
+                    ))
+                ) : (
+                    <p>추천 상품이 없습니다.</p>
+                )}
             </div>
         </section>
     );
